e632073d5e2c6ef331161ad7835d12c1ed5f2eec
AYOUB AMAZIGH <ayoubamazigh@tutanota.de>
2022-11-11 16:16:20 +0100

    dark-mode

--- assest/scripts/javascript/dark-mode.js
+++ assest/scripts/javascript/dark-mode.js
@@ -1,82 +1,24 @@
-if (window.localStorage.getItem('dark')){
-    if(window.localStorage.getItem('dark') == '1' ){
-
-        const h = document.querySelector('body')
-        const b1 = document.getElementById('darkmodeimage1')
-        const b2 = document.getElementById('darkmodeimage2')
-        b1.classList.add("darkmodebody")
-        b2.classList.add("darkmodebody")
-        h.classList.add("darkmodebody")
-        
+if (window.localStorage.getItem('theme')){
+    if(window.localStorage.getItem('theme') == 0){
+        document.write("<link rel='stylesheet' href='assest/css/light-mode.css' type='text/css'>")
+    }else if(window.localStorage.getItem('theme') == 1){
+        document.write("<link rel='stylesheet' href='assest/css/dark-mode.css' type='text/css'>")
     }else{
-
+        window.localStorage.setItem('theme', 0)
+        location.reload()
     }
-    
-    
 }else{
-    window.localStorage.setItem('dark', 0)
+    window.localStorage.setItem('theme', 0)
     location.reload()
 }
 
+function darkmode(){
 
-function darkmode (){
-
-    if(window.localStorage.getItem('dark') == '0' ){
-        const h = document.querySelector('body')
-        const b1 = document.getElementById('darkmodeimage1')
-        const b2 = document.getElementById('darkmodeimage2')
-        b1.classList.add("darkmodebody")
-        b2.classList.add("darkmodebody")
-        h.classList.add("darkmodebody")
-
-        window.localStorage.setItem('dark', 1)
-    }else{
-        const h = document.querySelector('body')
-        const b1 = document.getElementById('darkmodeimage1')
-        const b2 = document.getElementById('darkmodeimage2')
-        b1.classList.remove("darkmodebody")
-        b2.classList.remove("darkmodebody")
-        h.classList.remove("darkmodebody")
-
-        window.localStorage.setItem('dark', 0)
-    }
-
-
-
-
+        if(window.localStorage.getItem('theme') == 1){
+            window.localStorage.setItem('theme', 0)
+            location.reload()
+        }else if(window.localStorage.getItem('theme') == 0){
+            window.localStorage.setItem('theme', 1)
+            location.reload()
+        }
 }
-
-
-
-/*
-var a = 1;
-
-function darkmode()
-{
-
-
-    
-    if(a==1) {
-        // document.getElementById("navigationx").style.display="block";
-        const h = document.querySelector('body')
-        const b1 = document.getElementById('darkmodeimage1')
-        const b2 = document.getElementById('darkmodeimage2')
-        b1.classList.add("darkmodebody")
-        b2.classList.add("darkmodebody")
-        h.classList.add("darkmodebody")
-
-        return a=0;
-    }else{
-        // document.getElementById("navigationx").style.display="flex";
-        const h = document.querySelector('body')
-        const b1 = document.getElementById('darkmodeimage1')
-        const b2 = document.getElementById('darkmodeimage2')
-        b1.classList.remove("darkmodebody")
-        b2.classList.remove("darkmodebody")
-        h.classList.remove("darkmodebody")
-
-        return a=1;
-    }
-}
-
-*/
